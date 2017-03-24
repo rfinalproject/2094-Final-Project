f128156ccbf78facd2223ce281f7fc50ac630f73
unknown <>
2017-03-24 17:36:11 -0400

    first commit

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+##' Hello group!
+##'
+##' Hello group, this is going to be our first package.
+##' @title Hello group!
+##' @return
+##' @author
+##'
+
+hello <- function() {
+  print("Hello, group!")
+}
